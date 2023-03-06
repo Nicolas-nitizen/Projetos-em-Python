d9dcedbe0b8b940d7ad083a2d9192a6580d07cd7
Nicolas-nitizen <dracogames2001@gmail.com>
2023-03-06 19:28:06 -0300

    .

--- forca.py
+++ forca.py
@@ -1,4 +1,4 @@
-
+from random import randint
 def abertura():
     logo_tipo = "JOGO DA FORCA!"
     print("=" * 30)
@@ -8,16 +8,28 @@ def abertura():
 def jogar():
     abertura()
 
-    palavra_secreta = "python"
+    palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
     enforcou = False
     acertou = False
+    erros = 0
 
     #enquato nao enforcou e nao aceitou - no caso as duas variaveis enforcou e acertou é True
     while not enforcou and not acertou:
+        if chute in palavra_secreta:
+            chute = input("Qua é o seu chute? ").strip().lower()
+            pos = 0
+            for l in palavra_secreta:
+                if chute == l:
+                    letras_acertadas[pos] = l
 
-        chute = input("Qua é o seu chute? ")
+                pos += 1
+        else:
+            erros += 1
+        enforcou = erros == 6
+        print(letras_acertadas)
 
-        if chute ==
 
 if __name__ == "__main__":
     jogar()
