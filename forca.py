4799ccf7859d8bd2a6be2d66a13f8a0a2a16e338
Nicolas-nitizen <dracogames2001@gmail.com>
2023-03-06 20:13:56 -0300

    .

--- forca.py
+++ forca.py
@@ -17,18 +17,26 @@ def jogar():
 
     #enquato nao enforcou e nao aceitou - no caso as duas variaveis enforcou e acertou é True
     while not enforcou and not acertou:
+
+        chute = input("Qua é o seu chute? ").strip().lower()
+        pos = 0
         if chute in palavra_secreta:
-            chute = input("Qua é o seu chute? ").strip().lower()
-            pos = 0
             for l in palavra_secreta:
                 if chute == l:
                     letras_acertadas[pos] = l
-
                 pos += 1
         else:
             erros += 1
+
         enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+
+    if acertou:
+        print("Parabéns! Você ganhou")
+    elif enforcou:
+        print("Infelizmente não foi dessa vez! Tente novamente.")
+    print("FIM DO JOGO!!!")
 
 
 if __name__ == "__main__":
