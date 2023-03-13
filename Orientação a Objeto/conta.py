b683b50e46b4ac4fe32db4b8ba5d014799ed1542
Nicolas-nitizen <dracogames2001@gmail.com>
2023-03-13 11:57:12 -0300

    .

--- Orientação a Objeto/conta.py
+++ Orientação a Objeto/conta.py
@@ -1,20 +1,24 @@
-
 class Conta:
     def __int__(self, numero, titular, saldo, limite):
         print(f"Construindo Objeto ... {self}")
-        self.numero = 123
-        self.titular = "Nicolas"
-        self.saldo = 200.00
-        self.limite = 1000.0
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f"Saldo de {self.saldo} do titular {self.titular}")
+        print(f"Saldo de {self.__saldo} do titular {self.__titular}")
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
-if __name__ in "__main__":
-    Conta()
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.despositar(valor)
+
+
+usuario = Conta(123, "Nicolas", 66.0, 1000.0)
+usuario.extrato()
